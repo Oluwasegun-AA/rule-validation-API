74692dd622dda6c6c693b4748de2c2c870521741
Adepoju oluwasegun <oluwasegunadepoju@gmail.com>
2021-01-21 16:28:42 +0100

    implement validation controller

--- controllers/RuleValidatorController.js
+++ controllers/RuleValidatorController.js
@@ -1,7 +1,87 @@
+/* eslint-disable camelcase */
+import { isObject } from 'lodash';
+import {
+  responseHandler, status, statusCodes
+} from '../helpers';
+
+const response = (res, {
+  verdict, field, fieldValue, condition, condition_value
+}) => {
+  if (verdict) {
+    return responseHandler(res, {
+      code: statusCodes.success,
+      message: `field ${field} successfully validated.`,
+      status: status.success,
+      data: {
+        valudation: {
+          error: false,
+          field,
+          field_value: fieldValue,
+          condition,
+          condition_value
+        }
+      }
+    });
+  }
+  return responseHandler(res, {
+    code: statusCodes.badRequest,
+    message: `field ${field} failed validation.`,
+    status: status.error,
+    data: {
+      valudation: {
+        error: true,
+        field,
+        field_value: fieldValue,
+        condition,
+        condition_value
+      }
+    }
+  });
+};
+
 class Validator {
   static async validateRule(req, res) {
-    return res.status(200).json({
-      data: 'ok'
+    const { rule: { condition, field, condition_value }, data } = req.body;
+    let fieldValue = data;
+    const evaluateConditions = {
+      eq: (conditionValue, value) => value === conditionValue,
+      neq: (conditionValue, value) => value !== conditionValue,
+      gt: (conditionValue, value) => value > conditionValue,
+      gte: (conditionValue, value) => value >= conditionValue,
+      contains: (conditionValue, value) => value.includes(conditionValue)
+    };
+
+    const tree = field.split('.');
+    if (tree.length >= 1 && isObject(data[tree[0]]) && !Array.isArray(data[tree[0]])) {
+      if (JSON.stringify(data).indexOf(tree[tree.length - 1]) === -1) {
+        return responseHandler(res, {
+          code: statusCodes.badRequest,
+          message: `field ${field} is missing from data.`,
+          status: status.error,
+        });
+      }
+
+      for (let i = 0; i <= (tree.length - 1); i += 1) {
+        fieldValue = fieldValue[tree[i]];
+      }
+
+      const verdict = evaluateConditions[condition](condition_value, fieldValue);
+
+      return response(res, {
+        verdict, field, fieldValue, condition, condition_value
+      });
+    }
+    if (!data[field]) {
+      return responseHandler(res, {
+        code: statusCodes.badRequest,
+        message: `field ${field} is missing from data.`,
+        status: status.error,
+      });
+    }
+    fieldValue = fieldValue[field];
+    const verdict = evaluateConditions[condition](condition_value, data);
+    return response(res, {
+      verdict, field, fieldValue, condition, condition_value
     });
   }
 }
